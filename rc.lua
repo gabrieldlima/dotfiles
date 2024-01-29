87afea7b5eb6ddfd6f65e1c2181b4e6910099cba
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-01-29 18:59:32 -0300

    xterm --> wezterm

--- rc.lua
+++ rc.lua
@@ -47,7 +47,7 @@ end)
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "wezterm"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
