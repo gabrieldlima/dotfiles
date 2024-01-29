e38785b88cfb059ae21d47da6eb1f2dc04849cd8
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-01-29 19:00:33 -0300

    Master layout as first layout

--- rc.lua
+++ rc.lua
@@ -85,8 +85,8 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- Table of layouts to cover with awful.layout.inc, order matters.
 tag.connect_signal("request::default_layouts", function()
     awful.layout.append_default_layouts({
-        awful.layout.suit.floating,
         awful.layout.suit.tile,
+        awful.layout.suit.floating,
         awful.layout.suit.tile.left,
         awful.layout.suit.tile.bottom,
         awful.layout.suit.tile.top,
