f86612ceb7017ef8c20ad7b8aa9826b21db07b12
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-03-03 13:18:06 -0300

    Add mod.lua

---
+++ bindings/mod.lua
@@ -0,0 +1,16 @@
+return {
+  -- Modkeys
+  alt   = "Mod1",
+  super = "Mod4",
+  ctrl  = "Control",
+
+
+
+  leftclick     = 1,
+  midclick      = 2,
+  rightclick    = 3,
+  scrolldowm    = 4,
+  scrollup      = 5,
+  sidedownclick = 8,
+  sideupclick   = 9
+}
