ec904469402f6a8dda26e61bb30d390f6eeb07ed
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-03-03 13:50:29 -0300

    Dont check luarocks packages

--- rc.lua
+++ rc.lua
@@ -17,12 +17,6 @@ require("signals")
 local apps = require("apps")
 local mod  = require("bindings.mod")
 
-
--- awesome_mode: api-level=4:screen=on
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
-pcall(require, "luarocks.loader")
-
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
