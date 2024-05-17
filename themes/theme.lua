b431d234a72e867eac89d7667224a3a9eb72e7f1
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-05-17 19:20:09 -0300

    Fix whitespaces

--- themes/theme.lua
+++ themes/theme.lua
@@ -1,93 +1,93 @@
 -- NOTE: https://awesomewm.org/apidoc/documentation/06-appearance.md.html
-local gears                    = require("gears")
-local user                     = require("config.user")
-local colorscheme              = require("themes.colorsheme")
-local layout_path              = os.getenv("HOME") .. "/.config/awesome/assets/icons/layouts/"
-local titlebar_path            = os.getenv("HOME") .. "/.config/awesome/assets/icons/titlebar/"
+local gears         = require("gears")
+local user          = require("config.user")
+local color         = require("themes.colorsheme")
+local layout_path   = os.getenv("HOME") .. "/.config/awesome/assets/icons/layouts/"
+local titlebar_path = os.getenv("HOME") .. "/.config/awesome/assets/icons/titlebar/"
 
-local theme                    = {}
+local theme = {}
 
 -------------------------------------------------------------------------------
 -- Default variables
 -------------------------------------------------------------------------------
-theme.useless_gap              = 5
-theme.font                     = user.font
+theme.useless_gap                       = 5
+theme.font                              = user.font
 
-theme.bg_normal                = colorscheme.background
-theme.bg_focus                 = colorscheme.background
-theme.bg_urgent                = colorscheme.background
-theme.bg_minimize              = colorscheme.background
+theme.bg_normal                         = color.background
+theme.bg_focus                          = color.background
+theme.bg_urgent                         = color.background
+theme.bg_minimize                       = color.background
 
-theme.fg_normal                = colorscheme.foreground
-theme.fg_focus                 = colorscheme.foreground
-theme.fg_urgent                = colorscheme.red
-theme.fg_minimize              = colorscheme.foreground
+theme.fg_normal                         = color.foreground
+theme.fg_focus                          = color.foreground
+theme.fg_urgent                         = color.red
+theme.fg_minimize                       = color.foreground
 
-theme.border_width             = 3
-theme.border_color_active      = colorscheme.blue
-theme.border_color_normal      = colorscheme.background
+theme.border_width                      = 3
+theme.border_color_active               = color.blue
+theme.border_color_normal               = color.background
 
-theme.bg_systray               = colorscheme.background
+theme.bg_systray                        = color.background
 
 -------------------------------------------------------------------------------
 -- hotkeys
 -------------------------------------------------------------------------------
-theme.hotkeys_border_color     = colorscheme.blue
-theme.hotkeys_modifiers_fg     = colorscheme.foreground
-theme.hotkeys_font             = user.font
-theme.hotkeys_description_font = user.font
-theme.hotkeys_group_margin     = 10
+theme.hotkeys_border_color              = color.blue
+theme.hotkeys_modifiers_fg              = color.foreground
+theme.hotkeys_font                      = user.font
+theme.hotkeys_description_font          = user.font
+theme.hotkeys_group_margin              = 10
 
 -------------------------------------------------------------------------------
 -- taglist
 -------------------------------------------------------------------------------
-theme.taglist_font             = "FiraCode Nerd Font Bold 12"
-theme.taglist_bg_focus         = colorscheme.crust
-theme.taglist_fg_focus         = colorscheme.blue
-theme.taglist_fg_occupied      = colorscheme.text
-theme.taglist_fg_empty         = colorscheme.surface0
-theme.taglist_fg_urgent        = colorscheme.red
-theme.taglist_spacing          = 1
+theme.taglist_font                      = "FiraCode Nerd Font Bold 12"
+theme.taglist_bg_focus                  = color.crust
+theme.taglist_fg_focus                  = color.blue
+theme.taglist_fg_occupied               = color.text
+theme.taglist_fg_empty                  = color.surface0
+theme.taglist_fg_urgent                 = color.red
+theme.taglist_spacing                   = 1
 
 -------------------------------------------------------------------------------
 -- tasklist
 -------------------------------------------------------------------------------
-theme.tasklist_disable_icon    = true
+theme.tasklist_disable_icon             = true
 
 -------------------------------------------------------------------------------
 -- layout
 -------------------------------------------------------------------------------
-theme.layout_fairh             = layout_path .. "fairhw.png"
-theme.layout_fairv             = layout_path .. "fairvw.png"
-theme.layout_floating          = layout_path .. "floatingw.png"
-theme.layout_magnifier         = layout_path .. "magnifierw.png"
-theme.layout_max               = layout_path .. "maxw.png"
-theme.layout_fullscreen        = layout_path .. "fullscreenw.png"
-theme.layout_tilebottom        = layout_path .. "tilebottomw.png"
-theme.layout_tileleft          = layout_path .. "tileleftw.png"
-theme.layout_tile              = layout_path .. "tilew.png"
-theme.layout_tiletop           = layout_path .. "tiletopw.png"
-theme.layout_spiral            = layout_path .. "spiralw.png"
-theme.layout_dwindle           = layout_path .. "dwindlew.png"
-theme.layout_cornernw          = layout_path .. "cornernww.png"
-theme.layout_cornerne          = layout_path .. "cornernew.png"
-theme.layout_cornersw          = layout_path .. "cornersww.png"
-theme.layout_cornerse          = layout_path .. "cornersew.png"
+theme.layout_fairh                      = layout_path .. "fairhw.png"
+theme.layout_fairv                      = layout_path .. "fairvw.png"
+theme.layout_floating                   = layout_path .. "floatingw.png"
+theme.layout_magnifier                  = layout_path .. "magnifierw.png"
+theme.layout_max                        = layout_path .. "maxw.png"
+theme.layout_fullscreen                 = layout_path .. "fullscreenw.png"
+theme.layout_tilebottom                 = layout_path .. "tilebottomw.png"
+theme.layout_tileleft                   = layout_path .. "tileleftw.png"
+theme.layout_tile                       = layout_path .. "tilew.png"
+theme.layout_tiletop                    = layout_path .. "tiletopw.png"
+theme.layout_spiral                     = layout_path .. "spiralw.png"
+theme.layout_dwindle                    = layout_path .. "dwindlew.png"
+theme.layout_cornernw                   = layout_path .. "cornernww.png"
+theme.layout_cornerne                   = layout_path .. "cornernew.png"
+theme.layout_cornersw                   = layout_path .. "cornersww.png"
+theme.layout_cornerse                   = layout_path .. "cornersew.png"
 
 -------------------------------------------------------------------------------
 -- titlebar
 -------------------------------------------------------------------------------
-theme.titlebar_bg                                     = colorscheme.background
-theme.titlebar_bg_focus                               = colorscheme.background
+theme.titlebar_bg                       = color.background
+theme.titlebar_bg_focus                 = color.background
 -- Close Button
-theme.titlebar_close_button_normal                    = gears.color.recolor_image(titlebar_path .. "button.png", "#7f849c")
-theme.titlebar_close_button_focus                     = gears.color.recolor_image(titlebar_path .. "button.png", colorscheme.red)
-theme.titlebar_close_button_focus_hover               = titlebar_path .. "button_close.png"
+theme.titlebar_close_button_normal      = gears.color.recolor_image(titlebar_path .. "button.png", "#7f849c")
+theme.titlebar_close_button_focus       = gears.color.recolor_image(titlebar_path .. "button.png", color.red)
+theme.titlebar_close_button_focus_hover = titlebar_path .. "button_close.png"
 
 -------------------------------------------------------------------------------
 -- systray
 -------------------------------------------------------------------------------
-theme.bg_systray = colorscheme.base
+theme.bg_systray = color.base
 theme.systray_max_rows = nil
 theme.systray_icon_spacing = 10
 
