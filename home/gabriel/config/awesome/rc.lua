b92e3ce904d3d7422069094b60a463615ad2039a
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-05-31 21:35:08 -0300

    Update rc.lua

--- home/gabriel/config/awesome/rc.lua
+++ home/gabriel/config/awesome/rc.lua
@@ -23,8 +23,8 @@ require("awful.hotkeys_popup.keys")
 
 -- Autostart script
 require("awful").spawn.with_shell(os.getenv("HOME") .. "/.config/awesome/config/autostart.sh")
--- require("awful").spawn("openrgb -d 0 -m off", false)
--- require("awful").spawn("openrgb -d 1 -m off", false)
+require("awful").spawn("openrgb -d 0 -m off", false)
+require("awful").spawn("openrgb -d 1 -m off", false)
 
 -- Garbage collection
 require("gears").timer {
