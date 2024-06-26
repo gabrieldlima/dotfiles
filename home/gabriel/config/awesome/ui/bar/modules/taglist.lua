254ad409027e83a93b8d16c84faadcccd0381912
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-06-26 20:48:32 -0300

    Create taglist buttons

--- home/gabriel/config/awesome/ui/bar/modules/taglist.lua
+++ home/gabriel/config/awesome/ui/bar/modules/taglist.lua
@@ -3,31 +3,77 @@ local mod     = require("config.binds.modkeys")
 local helpers = require("helpers")
 local colors  = require("themes.colorsheme")
 
-local taglist = function(s)
+local buttons = {
+  awful.button {
+    description = "move_to_tag",
+    modifiers = { mod.super },
+    button = mod.leftclick,
+    on_press = function (t)
+      if client.focus then
+        client.focus:move_to_tag(t)
+      end
+    end,
+    on_release = nil,
+  },
+
+  awful.button {
+    description = "toggle_tag",
+    modifiers = { mod.super },
+    button = mod.rightclick,
+    on_press = function (t)
+      if client.focus then
+        client.focus:toggle_tag(t)
+      end
+    end,
+    on_release = nil,
+  },
+
+  awful.button {
+    description = "viewtoggle",
+    modifiers = {},
+    button = mod.rightclick,
+    on_press = function (t)
+      awful.tag.viewtoggle(t)
+    end,
+    on_release = nil,
+  },
+
+  awful.button {
+    description = "viewprev",
+    modifiers = {},
+    button = mod.scrollup,
+    on_press = function (t)
+      awful.tag.viewprev(t.screen)
+    end,
+    on_release = nil,
+  },
+
+  awful.button {
+    description = "viewnext",
+    modifiers = {},
+    button = mod.scrolldowm,
+    on_press = function (t)
+      awful.tag.viewnext(t.screen)
+    end,
+    on_release = nil,
+  },
+
+  awful.button {
+    description = "view_only",
+    modifiers = {},
+    button = mod.leftclick,
+    on_press = function (t)
+      t:view_only()
+    end,
+    on_release = nil,
+  },
+}
+
+local taglist = function (s)
   local taglist_widget = awful.widget.taglist {
     screen  = s,
     filter  = awful.widget.taglist.filter.all,
-    buttons = {
-      awful.button({ mod.super }, mod.leftclick,
-        function(t)
-          if client.focus then
-            client.focus:move_to_tag(t)
-          end
-        end
-      ),
-      awful.button(
-        { mod.super }, mod.rightclick,
-        function(t)
-          if client.focus then
-            client.focus:toggle_tag(t)
-          end
-        end
-      ),
-      awful.button({}, mod.rightclick, awful.tag.viewtoggle),
-      awful.button({}, mod.scrollup,function(t) awful.tag.viewprev(t.screen) end),
-      awful.button({}, mod.scrolldowm,function(t) awful.tag.viewnext(t.screen) end),
-      awful.button({}, mod.leftclick, function(t) t:view_only() end),
-    }
+    buttons = buttons
   }
 
   return helpers.cbackground(helpers.margin(taglist_widget, 4, 4, 4, 4), helpers.rrect(4), colors.background)
