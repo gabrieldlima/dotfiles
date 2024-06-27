aec9a711fc46c5b3412be15880071b969f49762b
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-06-27 20:37:41 -0300

    feat: add Xresources config in Nix

--- home/gabriel/config/awesome/default.nix
+++ home/gabriel/config/awesome/default.nix
@@ -21,6 +21,7 @@ let
 in {
   imports = [
     ../picom.nix
+    ../xresources.nix
   ];
 
   home.packages = with pkgs; [
@@ -28,50 +29,6 @@ in {
     xsel
     xwallpaper
   ];
-
-  xresources = {
-    properties = {
-      "XTerm*faceName" = "FiraCode Nerd Font:size=12";
-      "Xcursor.theme" = "catppuccin-mocha-light-cursors";
-      "Xcursor.size" = 16;
-    };
-    extraConfig = ''
-      *background: #050508
-      *foreground: #CDD6F4
-
-      ! black
-      *color0: #45475A
-      *color8: #585B70
-
-      ! red
-      *color1: #F38BA8
-      *color9: #F38BA8
-
-      ! green
-      *color2: #A6E3A1
-      *color10: #A6E3A1
-
-      ! yellow
-      *color3: #F9E2AF
-      *color11: #F9E2AF
-
-      ! blue
-      *color4: #89B4FA
-      *color12: #89B4FA
-
-      ! magenta
-      *color5: #F5C2E7
-      *color13: #F5C2E7
-
-      ! cyan
-      *color6: #94E2D5
-      *color14: #94E2D5
-
-      ! white
-      *color7: #BAC2DE
-      *color15: #A6ADC8
-    '';
-  };
 
   xsession.windowManager.awesome = {
     enable = true;
