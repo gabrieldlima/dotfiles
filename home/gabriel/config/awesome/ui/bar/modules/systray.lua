9e03de05faf0691c481233cf173f59bc41cf7596
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-06-26 22:17:12 -0300

    var rename

--- home/gabriel/config/awesome/ui/bar/modules/systray.lua
+++ home/gabriel/config/awesome/ui/bar/modules/systray.lua
@@ -1,6 +1,6 @@
 local wibox   = require("wibox")
 local helpers = require("helpers")
-local color   = require("themes.colorsheme")
+local colors  = require("themes.colorsheme")
 
 local font      = "FiraCode Nerd Font Bold 12"
 
@@ -9,18 +9,18 @@ local font      = "FiraCode Nerd Font Bold 12"
 local show_icon = ""
 local hide_icon = ""
 
-local icon = helpers.textbox(color.foreground, font, show_icon)
+local icon = helpers.textbox(colors.foreground, font, show_icon)
 local tray_toggle = helpers.margin(icon, 4, 4, 4, 4)
 local systray = helpers.margin({ base_size = 20, widget = wibox.widget.systray }, 5, 5, 5, 5, false)
-local togglableSystray = helpers.cbackground({ systray, tray_toggle, layout = wibox.layout.fixed.horizontal }, helpers.rrect(4), color.background)
+local togglableSystray = helpers.cbackground({ systray, tray_toggle, layout = wibox.layout.fixed.horizontal }, helpers.rrect(4), colors.background)
 
 tray_toggle:connect_signal("button::press", function(_, _, _, button)
   if button == 1 then
     systray.visible = not systray.visible
     if systray.visible then
-      icon.markup = helpers.mtext(color.foreground, font, hide_icon)
+      icon.markup = helpers.mtext(colors.foreground, font, hide_icon)
     else
-      icon.markup = helpers.mtext(color.foreground, font, show_icon)
+      icon.markup = helpers.mtext(colors.foreground, font, show_icon)
     end
   end
 end)
