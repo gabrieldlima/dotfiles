9e027eabf5b7ee54f0f58ac817ff4c66ae03e218
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-07-13 16:18:49 -0300

    hyprland: update animations section

--- home/gabriel/config/hyprland/default.nix
+++ home/gabriel/config/hyprland/default.nix
@@ -107,22 +107,15 @@
         enabled = true;
         first_launch_animation = true;
         bezier = [
-          "easein,0.11, 0, 0.5, 0"
-          "easeout,0.5, 1, 0.89, 1"
-          "easeinback,0.36, 0, 0.66, -0.56"
-          "easeoutback,0.34, 1.56, 0.64, 1"
+          "myBezier, 0.05, 0.9, 0.1, 1.05"
         ];
         animation = [
-          "windowsIn,1,3,easeoutback,slide"
-          "windowsOut,1,3,easeinback,slide"
-          "windowsMove,1,3,easeoutback"
-          "workspaces,1,2,easeoutback,slide"
-          "fadeIn,1,3,easeout"
-          "fadeOut,1,3,easein"
-          "fadeSwitch,1,3,easeout"
-          "fadeShadow,1,3,easeout"
-          "fadeDim,1,3,easeout"
-          "border,1,3,easeout"
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
         ];
       };
 
