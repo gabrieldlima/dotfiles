fa916067a93797a251dbdc9f495a1c5d0e658724
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-09-22 11:27:45 -0300

    fix wrong microcode setting

--- hosts/aorus/hardware-configuration.nix
+++ hosts/aorus/hardware-configuration.nix
@@ -50,5 +50,5 @@
   powerManagement.cpuFreqGovernor = "performance";
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
