1717979214031779d9f283e0dd0b1630a171ecb5
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-09-22 13:49:45 -0300

    awesomewm: update cpu widget

--- home/gabriel/config/awesome/ui/bar/modules/cpu.lua
+++ home/gabriel/config/awesome/ui/bar/modules/cpu.lua
@@ -4,48 +4,38 @@ local colors    = require("theme.colorsheme")
 local helpers   = require("helpers")
 local user      = require("config.user")
 
-local arc_cpu = {}
-local text_cpu = {}
+-- local cpu_widget = wibox.widget {
+--   {
+--     {
+--       align = "center",
+--       valign = "center",
+--       widget = wibox.widget.textbox
+--     },
+--     widget = wibox.container.background
+--   },
+--   mix_value = 0,
+--   max_value = 100,
+--   rounded_edge = false,
+--   thickness = 6,
+--   start_angle = 4.71238898,
+--   forced_height = 22,
+--   forced_width = 22,
+--   bg = colors.mantle,
+--   widget = wibox.container.arcchart
+-- }
 
-local text = wibox.widget {
-  align = "center",
-  valign = "center",
+local cmd = [[
+  bash -c "echo $(top -bn1 | grep 'Cpu(s)' | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | awk '{print int(100 - $1)\"%\"}')"
+]]
+
+local cpu_widget = wibox.widget {
   widget = wibox.widget.textbox
 }
 
-local textbg = wibox.widget {
-  text,
-  widget = wibox.container.background
-}
-
-arc_cpu = wibox.widget {
-  textbg,
-  mix_value = 0,
-  max_value = 100,
-  rounded_edge = false,
-  thickness = 6,
-  start_angle = 4.71238898,
-  forced_height = 22,
-  forced_width = 22,
-  bg = colors.mantle,
-  widget = wibox.container.arcchart
-}
-
-text_cpu = wibox.widget {
-  widget = wibox.widget.textbox
-}
-
-watch(user.config.home .. "/.config/awesome/config/cpu.sh", 2,
+watch(cmd, 2,
   function(widget, stdout)
     widget.markup = helpers.mtext(colors.green, user.config.font, " " .. stdout)
   end,
-  text_cpu)
+  cpu_widget)
 
--- watch(user.config.home .. "/.config/awesome/config/cpu.sh", 2,
---   function(widget, stdout)
---     widget.value = stdout
---     widget.colors = { colors.green }
---   end,
---   arc_cpu)
-
-return helpers.cbackground(helpers.margin(text_cpu, 4, 4, 4, 4), helpers.rrect(4), colors.background)
+return helpers.cbackground(helpers.margin(cpu_widget, 4, 4, 4, 4), helpers.rrect(4), colors.background)
