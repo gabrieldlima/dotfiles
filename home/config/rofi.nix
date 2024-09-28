b2e14da5cf2ecf50372bb5f930842b63bbdb1ad0
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-09-28 20:18:39 -0300

    rofi: upate config

--- home/config/rofi.nix
+++ home/config/rofi.nix
@@ -5,5 +5,23 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+
+    font = "FiraCode Nerd Font Bold 12";
+
+    extraConfig = {
+      modi= "run,drun,window";
+      icon-theme= "Papirus";
+      show-icons= true;
+      terminal= "alacritty";
+      drun-display-format= "{icon} {name}";
+      location= 0;
+      disable-history= false;
+      hide-scrollbar= true;
+      display-drun= "󰀘 Apps";
+      display-run= " Run";
+      display-window= "󰕰 Window";
+      display-Network= "󰤨 Network";
+      sidebar-mode= true;
+    };
   };
 }
