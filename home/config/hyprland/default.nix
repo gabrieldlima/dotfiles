bb581abef941c82b9d1aa3ababd8690a4db86062
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-10-12 16:02:31 -0300

    hyprland: setup some env vars

--- home/config/hyprland/default.nix
+++ home/config/hyprland/default.nix
@@ -56,6 +56,22 @@ in {
     package = pkgs.hyprland;
     xwayland.enable = true;
 
+    extraConfig = ''
+      env = GDK_BACKEND,wayland,x11,*
+      env = QT_QPA_PLATFORM,wayland;xcb
+      env = SDL_VIDEODRIVER,wayland
+      env = CLUTTER_BACKEND,wayland
+
+      env = XDG_CURRENT_DESKTOP,Hyprland
+      env = XDG_SESSION_TYPE,wayland
+      env = XDG_SESSION_DESKTOP,Hyprland
+
+      env = QT_AUTO_SCREEN_SCALE_FACTOR,1
+      env = QT_QPA_PLATFORM,wayland;xcb
+      env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
+      env = QT_QPA_PLATFORMTHEME,qt5ct
+    '';
+
     settings = {
       # Monitors
       # ========================================================================
