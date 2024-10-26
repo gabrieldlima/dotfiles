5ca094a56d227485e5be15212a70d37402beb4b4
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-10-26 18:41:41 -0300

    font: use FiraCode as Nerd Font

--- hosts/aorus/default.nix
+++ hosts/aorus/default.nix
@@ -85,7 +85,7 @@
   fonts = {
     enableDefaultPackages = true;
     packages = with pkgs; [
-      (nerdfonts.override { fonts = [ "Iosevka" ]; })
+      (nerdfonts.override { fonts = [ "FiraCode" ]; })
     ];
   };
 
