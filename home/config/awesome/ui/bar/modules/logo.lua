0ad6bb4977a3ead52e287bccda6c1f29941555ab
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-11-24 19:50:51 -0300

    awesomewm: update logo widget

--- home/config/awesome/ui/bar/modules/logo.lua
+++ home/config/awesome/ui/bar/modules/logo.lua
@@ -1,7 +1,21 @@
+local awful   = require("awful")
 local wibox   = require("wibox")
+local mod     = require("binds.mod")
 local helpers = require("helpers")
 local colors  = require("theme.colorsheme")
 local user    = require("config.user")
+
+local buttons = {
+  awful.button {
+    description = "open launcher",
+    modifiers = { },
+    button = mod.leftclick,
+    on_press = function()
+      awful.spawn(user.apps.launcher)
+    end,
+    on_release = nil,
+  },
+}
 
 local logo = wibox.widget {
   image = helpers.recolor_image(user.config.home .. "/.config/awesome/theme/assets/logos/nix.svg", colors.blue),
@@ -10,6 +24,7 @@ local logo = wibox.widget {
   forced_height = 28,
   forced_width = 28,
   resize = true,
+  buttons = buttons,
   widget = wibox.widget.imagebox,
 }
 local mylogo = helpers.cbackground(helpers.margin(logo, 4, 4, 0, 0), helpers.rrect(4), colors.background)
