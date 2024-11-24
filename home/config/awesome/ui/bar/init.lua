abf7c8eaaf937cbda42dfff0b442d8b4ce9bf112
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-11-24 11:43:19 -0300

    enable awesomewm/picom/rofi rounded borders

--- home/config/awesome/ui/bar/init.lua
+++ home/config/awesome/ui/bar/init.lua
@@ -27,17 +27,16 @@ awful.screen.connect_for_each_screen(function(s)
     screen   = s,
     visible  = true,
     ontop    = false,
-    width    = 1920,
-    -- width    = 1900,
+    width    = 1900,
     height   = 40,
     type     = "dock",
-    -- shape    = helpers.rrect(10),
-    -- margins  = {
-    --   left = 0,
-    --   right = 0,
-    --   top = 8,
-    --   bottom = 0,
-    -- },
+    shape    = helpers.rrect(10),
+    margins  = {
+      left = 0,
+      right = 0,
+      top = 8,
+      bottom = 0,
+    },
   }
 
   s.bar:setup {
