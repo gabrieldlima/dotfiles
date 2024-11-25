633fe91bd2f06caa323b06029028b2605d25f231
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-11-25 14:12:22 -0300

    awesomewm: update catppuccin_mocha.lua

--- home/config/awesome/theme/colorsheme/catppuccin_mocha.lua
+++ home/config/awesome/theme/colorsheme/catppuccin_mocha.lua
@@ -1,19 +1,20 @@
 local colorscheme = {
   background = "#000000",
   foreground = "#cdd6f4",
-  red        = "#f38ba8",
-  green      = "#a6e3a1",
-  yellow     = "#f9e2af",
-  blue       = "#89b4fa",
+  rosewater  = "#f5e0dc",
+  flamingo   = "#f2cdcd",
   pink       = "#f5c2e7",
-  aqua       = "#94e2d5", -- teal
-  orange     = "#fab387", -- peach
-  purple     = "#cba6f7", -- mauve
-  cyan       = "#74c7ec", -- sapphire
+  mauve      = "#cba6f7",
+  red        = "#f38ba8",
+  marron     = "#eba0ac",
+  peach      = "#fab387",
+  yellow     = "#f9e2af",
+  green      = "#a6e3a1",
+  teal       = "#94e2d5",
+  sky        = "#89dceb",
+  sapphire   = "#74c7ec",
+  blue       = "#89b4fa",
   lavender   = "#b4befe",
-  base       = "#1e1e2e",
-  mantle     = "#181825",
-  crust      = "#11111b",
   subtext1   = "#bac2de",
   subtext0   = "#a6adc8",
   overlay2   = "#9399b2",
@@ -22,6 +23,9 @@ local colorscheme = {
   surface2   = "#585b70",
   surface1   = "#45475a",
   surface0   = "#313244",
+  base       = "#1e1e2e",
+  mantle     = "#181825",
+  crust      = "#11111b",
 }
 
 return colorscheme
