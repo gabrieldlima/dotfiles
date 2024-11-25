1f099867507c197630ef5aeec4a89dd951db9b34
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-11-25 13:03:00 -0300

    awesomewm: moved signals to one file only

---
+++ home/config/awesome/signal.lua
@@ -0,0 +1,56 @@
+local awful = require("awful")
+local ruled = require("ruled")
+local naughty = require("naughty")
+
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+naughty.connect_signal("request::display_error", function(message, startup)
+  naughty.notification {
+    urgency = "critical",
+    title   = "Oops, an error happened"..(startup and " during startup!" or "!"),
+    message = message
+  }
+end)
+
+-- Enable sloppy focus, so that focus follows mouse.
+client.connect_signal("mouse::enter", function(c)
+  c:activate { context = "mouse_enter", raise = false }
+end)
+
+ruled.notification.connect_signal('request::rules', function()
+  -- All notifications will match this rule.
+  ruled.notification.append_rule {
+    rule       = { },
+    properties = {
+      screen           = awful.screen.preferred,
+      implicit_timeout = 5,
+    }
+  }
+end)
+
+naughty.connect_signal("request::display", function(n)
+  naughty.layout.box { notification = n }
+end)
+
+--
+-- [[ Tag layout ]]
+--
+-- Table of layouts to cover with awful.layout.inc, order matters.
+tag.connect_signal("request::default_layouts", function()
+  awful.layout.append_default_layouts({
+    awful.layout.suit.tile,
+    awful.layout.suit.floating,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
+    awful.layout.suit.spiral,
+    awful.layout.suit.spiral.dwindle,
+    awful.layout.suit.max,
+    awful.layout.suit.max.fullscreen,
+    awful.layout.suit.magnifier,
+    awful.layout.suit.corner.nw,
+  })
+end)
+-- }}}
