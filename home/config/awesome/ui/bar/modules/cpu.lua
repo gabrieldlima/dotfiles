b7680c2d25dcc1db27a49a13238ad0819b154318
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-12-07 14:19:30 -0300

    awesomewm: update cpu widget

--- home/config/awesome/ui/bar/modules/cpu.lua
+++ home/config/awesome/ui/bar/modules/cpu.lua
@@ -34,9 +34,8 @@ local cpu_widget = wibox.widget {
 
 watch(cmd, 2,
   function(widget, stdout)
-    widget.markup = helpers.mtext(colors.background, user.config.font, " ") ..
-                    helpers.mtext(colors.background, user.config.font, stdout)
+    widget.markup = helpers.mtext(colors.green, "FiraCode Nerd Font Bold 12", " ") .. helpers.mtext(colors.foreground, user.config.font, stdout)
   end,
   cpu_widget)
 
-return helpers.cbackground(helpers.margin(cpu_widget, 4, 4, 4, 4), helpers.rrect(4), colors.blue)
+return helpers.cbackground(helpers.margin(cpu_widget, 4, 4, 4, 4), helpers.rrect(4), colors.background)
