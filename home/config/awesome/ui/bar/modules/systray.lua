5e8a5dc7b63e305fecb36ebba3b6d421e13a203d
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-12-07 14:19:16 -0300

    awesomewm: update systray widget

--- home/config/awesome/ui/bar/modules/systray.lua
+++ home/config/awesome/ui/bar/modules/systray.lua
@@ -10,18 +10,18 @@ local font = user.config.font
 local show_icon = ""
 local hide_icon = ""
 
-local icon = helpers.textbox(colors.background, font, show_icon)
+local icon = helpers.textbox(colors.foreground, font, show_icon)
 local tray_toggle = helpers.margin(icon, 4, 4, 4, 4)
 local systray = helpers.margin({ base_size = 20, widget = wibox.widget.systray }, 2, 2, 2, 2, false)
-local togglableSystray = helpers.cbackground({ systray, tray_toggle, layout = wibox.layout.fixed.horizontal }, helpers.rrect(4), colors.blue)
+local togglableSystray = helpers.cbackground({ systray, tray_toggle, layout = wibox.layout.fixed.horizontal }, helpers.rrect(4), colors.background)
 
 tray_toggle:connect_signal("button::press", function(_, _, _, button)
   if button == 1 then
     systray.visible = not systray.visible
     if systray.visible then
-      icon.markup = helpers.mtext(colors.background, font, hide_icon)
+      icon.markup = helpers.mtext(colors.foreground, font, hide_icon)
     else
-      icon.markup = helpers.mtext(colors.background, font, show_icon)
+      icon.markup = helpers.mtext(colors.foreground, font, show_icon)
     end
   end
 end)
