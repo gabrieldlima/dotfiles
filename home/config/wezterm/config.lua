16b0260a7b14b06a876fc2b4d0a9cb05912bf7f7
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-12-28 13:18:33 -0300

    wezterm: OpenGL as backend

--- home/config/wezterm/config.lua
+++ home/config/wezterm/config.lua
@@ -25,6 +25,7 @@ M.window_background_opacity = 0.95
 M.enable_tab_bar = true
 M.use_fancy_tab_bar = false
 M.hide_tab_bar_if_only_one_tab = false
+M.front_end = "OpenGL"
 M.max_fps = 120
 M.animation_fps = 120
 
