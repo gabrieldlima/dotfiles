ed7550ecb9466684bb7e577debdaa06c66cc9754
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-02-26 12:13:12 -0300

    hyprland: new monitor and config update

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -11,17 +11,41 @@
   pkgs,
   ...
 }: let
+  # ====================================================================== #
+  # COLORS                                                                 #
+  # ====================================================================== #
   red  = "rgba(f38ba8ff)";
   blue = "rgba(89b4faff)";
   text = "rgba(cdd6f4ff)";
   base = "rgba(000000ff)";
+
+  # ====================================================================== #
+  # DEFAULT APPS                                                           #
+  # ====================================================================== #
+  browser  = "qutebrowser";
+  launcher = "rofi -show drun";
+  terminal = "ghostty";
+
+  # ====================================================================== #
+  # MODKEYS                                                                #
+  # ====================================================================== #
+  modM = "SUPER";
+  modS = "SUPER + SHIFT";
+  modC = "SUPER + CONTROL";
+  modA = "SUPER + ALT";
 in {
+  # ====================================================================== #
+  # IMPORTS                                                                #
+  # ====================================================================== #
   imports = [
-    ../waybar
+    # ../waybar
   ];
 
-  home.packages = with pkgs; [
-    swww
+  # ====================================================================== #
+  # WAYLAND-ONLY PACKAGES                                                  #
+  # ====================================================================== #
+  home.packages = [
+    pkgs.swww
   ];
 
   wayland.windowManager.hyprland = {
@@ -32,14 +56,13 @@ in {
       # ====================================================================== #
       # MONITORS                                                               #
       # ====================================================================== #
-      monitor = "HDMI-A-1,1920x1080@60,0x0,1";
+      monitor="DP-2,2560x1440@165,0x0,1";
 
       # ====================================================================== #
       # AUTOSTART                                                              #
       # ====================================================================== #
       exec-once = [
         "swww-daemon"
-        "waybar"
       ];
 
       # ====================================================================== #
@@ -64,11 +87,58 @@ in {
       };
 
       # ====================================================================== #
+      # DECORATION                                                             #
+      # ====================================================================== #
+      decoration = {
+        blur = {
+          enabled = false;
+        };
+      };
+
+      # ====================================================================== #
+      # ANIMATIONS                                                             #
+      # ====================================================================== #
+      animations = {
+        bezier = [
+          "easeOutQuint,0.23,1,0.32,1"
+          "easeInOutCubic,0.65,0.05,0.36,1"
+          "linear,0,0,1,1"
+          "almostLinear,0.5,0.5,0.75,1.0"
+          "quick,0.15,0,0.1,1"
+        ];
+
+        animation = [
+          "global, 1, 10, default"
+          "border, 1, 5.39, easeOutQuint"
+          "windows, 1, 4.79, easeOutQuint"
+          "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+          "windowsOut, 1, 1.49, linear, popin 87%"
+          "fadeIn, 1, 1.73, almostLinear"
+          "fadeOut, 1, 1.46, almostLinear"
+          "fade, 1, 3.03, quick"
+          "layers, 1, 3.81, easeOutQuint"
+          "layersIn, 1, 4, easeOutQuint, fade"
+          "layersOut, 1, 1.5, linear, fade"
+          "fadeLayersIn, 1, 1.79, almostLinear"
+          "fadeLayersOut, 1, 1.39, almostLinear"
+          "workspaces, 1, 1.94, almostLinear, fade"
+          "workspacesIn, 1, 1.21, almostLinear, fade"
+          "workspacesOut, 1, 1.94, almostLinear, fade"
+        ];
+      };
+
+      # ====================================================================== #
       # INPUT                                                                  #
       # ====================================================================== #
       input = {
-        kb_layout = "us";
         kb_options = "caps:swapescape";
+      };
+
+      # ====================================================================== #
+      # MISC                                                                   #
+      # ====================================================================== #
+      misc = {
+        font_family = "JetBrains Mono";
       };
 
       # ====================================================================== #
@@ -83,30 +153,27 @@ in {
 
         "workspace 9, class:(steam)"
         "float, class:(steam)"
+
+        # Ignore maximize requests from apps. You'll probably like this.
+        "suppressevent maximize, class:.*"
+
+        # Fix some dragging issues with XWayland
+        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
 
       # ====================================================================== #
       # WORKSPACES RULES                                                       #
       # ====================================================================== #
       workspace = [
-        "9, gapsin:0, gapsout:0, bordersize:0, border:false, shadow:false, rounding:false, decorate:false"
+        "9, gapsin:0, gapsout:0, shadow:false, rounding:false, decorate:false"
       ];
 
       # ====================================================================== #
-      # BINDS                                                                  #
+      # BINDS (KEYBOARD)                                                       #
       # ====================================================================== #
-      bind = let
-        browser  = "qutebrowser";
-        launcher = "rofi -show drun";
-        terminal = "ghostty";
-
-        modM = "SUPER";
-        modS = "SUPER + SHIFT";
-        modC = "SUPER + CONTROL";
-        modA = "SUPER + ALT";
-      in [
+      bind = [
         "${modM}, RETURN, exec, ${terminal}"
-        "${modM}, R , exec, ${launcher}"
+        "${modM}, P , exec, ${launcher}"
         "${modM}, B , exec, ${browser}"
 
         "${modS}, Q, exit"
@@ -149,112 +216,21 @@ in {
 
         "${modS}, RETURN, layoutmsg, swapwithmaster"
       ];
+
+      binde = [
+        "${modC}, h, resizeactive, -50 0"
+        "${modC}, l, resizeactive, 50 0"
+        "${modC}, k, resizeactive, 0 -50"
+        "${modC}, j, resizeactive, 0 50"
+      ];
+
+      # ====================================================================== #
+      # BINDS (MOUSE)                                                          #
+      # ====================================================================== #
+      bindm = [
+        "${modM}, mouse:272, movewindow"
+        "${modM}, mouse:273, resizewindow"
+      ];
     };
-
-    extraConfig = ''
-      #############################
-      ### ENVIRONMENT VARIABLES ###
-      #############################
-      env = XCURSOR_SIZE,24
-      env = HYPRCURSOR_SIZE,24
-
-
-      #####################
-      ### LOOK AND FEEL ###
-      #####################
-      decoration {
-          rounding = 10
-          rounding_power = 2
-
-          # Change transparency of focused and unfocused windows
-          active_opacity = 1.0
-          inactive_opacity = 1.0
-
-          shadow {
-              enabled = true
-              range = 4
-              render_power = 3
-              color = rgba(1a1a1aee)
-          }
-
-          blur {
-              enabled = true
-              size = 3
-              passes = 1
-
-              vibrancy = 0.1696
-          }
-      }
-
-      animations {
-          enabled = yes, please :)
-
-          bezier = easeOutQuint,0.23,1,0.32,1
-          bezier = easeInOutCubic,0.65,0.05,0.36,1
-          bezier = linear,0,0,1,1
-          bezier = almostLinear,0.5,0.5,0.75,1.0
-          bezier = quick,0.15,0,0.1,1
-
-          animation = global, 1, 10, default
-          animation = border, 1, 5.39, easeOutQuint
-          animation = windows, 1, 4.79, easeOutQuint
-          animation = windowsIn, 1, 4.1, easeOutQuint, popin 87%
-          animation = windowsOut, 1, 1.49, linear, popin 87%
-          animation = fadeIn, 1, 1.73, almostLinear
-          animation = fadeOut, 1, 1.46, almostLinear
-          animation = fade, 1, 3.03, quick
-          animation = layers, 1, 3.81, easeOutQuint
-          animation = layersIn, 1, 4, easeOutQuint, fade
-          animation = layersOut, 1, 1.5, linear, fade
-          animation = fadeLayersIn, 1, 1.79, almostLinear
-          animation = fadeLayersOut, 1, 1.39, almostLinear
-          animation = workspaces, 1, 1.94, almostLinear, fade
-          animation = workspacesIn, 1, 1.21, almostLinear, fade
-          animation = workspacesOut, 1, 1.94, almostLinear, fade
-      }
-
-      dwindle {
-          pseudotile = true # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-          preserve_split = true # You probably want this
-      }
-
-      misc {
-          force_default_wallpaper = -1 # Set to 0 or 1 to disable the anime mascot wallpapers
-          disable_hyprland_logo = false # If true disables the random hyprland logo / anime girl background. :(
-      }
-
-      gestures {
-          workspace_swipe = false
-      }
-
-      device {
-          name = epic-mouse-v1
-          sensitivity = -0.5
-      }
-
-      # Example special workspace (scratchpad)
-      bind = SUPER, S, togglespecialworkspace, magic
-      bind = SUPER SHIFT, S, movetoworkspace, special:magic
-
-      # Move/resize windows with mainMod + LMB/RMB and dragging
-      bindm = SUPER, mouse:272, movewindow
-      bindm = SUPER, mouse:273, resizewindow
-      #
-
-      ##############################
-      ### WINDOWS AND WORKSPACES ###
-      ##############################
-      # Example windowrule v1
-      # windowrule = float, ^(kitty)$
-
-      # Example windowrule v2
-      # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
-
-      # Ignore maximize requests from apps. You'll probably like this.
-      windowrulev2 = suppressevent maximize, class:.*
-
-      # Fix some dragging issues with XWayland
-      windowrulev2 = nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0
-    '';
   };
 }
