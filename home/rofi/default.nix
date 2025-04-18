b59af8a4ad777edd10a356b655d3a689f68e828d
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-04-17 23:18:43 -0300

    rofi: change colors and layout

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -19,18 +19,18 @@
     font = "JetBrains Mono ExtraBold 10";
 
     extraConfig = {
-      modi = "run,drun,window";
+      modi = "drun,run,window";
       icon-theme = "Papirus";
-      show-icons = true;
-      terminal = "wezterm";
+      show-icons = false;
+      terminal = "ghostty";
       drun-display-format = "{icon} {name}";
       location = 0;
-      disable-history = false;
+      disable-history = true;
       hide-scrollbar = true;
-      display-drun = "󰀘 Apps";
-      display-run = " Run";
-      display-window = "󰕰 Window";
-      display-Network = "󰤨 Network";
+      display-drun = "drun";
+      # display-run = " Run";
+      # display-window = "󰕰 Window";
+      # display-Network = "󰤨 Network";
       sidebar-mode = true;
     };
 
@@ -38,93 +38,93 @@
       inherit (config.lib.formats.rasi) mkLiteral;
     in {
       "*" = {
-        width             = mkLiteral "800";
+        width             = mkLiteral "400";
       };
       "#element-text, element-icon , mode-switcher" = {
         background-color  = mkLiteral "inherit";
         text-color        = mkLiteral "inherit";
       };
       "#window" = {
-        background-color  = mkLiteral "#000000";
+        background-color  = mkLiteral "#0f0f0f";
         border            = mkLiteral "2px";
-        border-color      = mkLiteral "#89b4fa";
+        border-color      = mkLiteral "#458588";
         border-radius     = mkLiteral "0px";
-        height            = mkLiteral "400px";
+        height            = mkLiteral "500px";
       };
       "#mainbox" = {
-        background-color  = mkLiteral "#000000";
+        background-color  = mkLiteral "#0f0f0f";
       };
       "#inputbar" = {
         children          = mkLiteral "[prompt,entry]";
-        background-color  = mkLiteral "#000000";
-        border-radius     = mkLiteral "4px";
+        background-color  = mkLiteral "#0f0f0f";
+        border-radius     = mkLiteral "0px";
         padding           = mkLiteral "2px";
       };
       "#prompt" = {
-        background-color  = mkLiteral "#89b4fa";
-        border-radius     = mkLiteral "4px";
+        background-color  = mkLiteral "#458588";
+        border-radius     = mkLiteral "0px";
         margin            = mkLiteral "20px 0px 0px 20px";
         padding           = mkLiteral "6px";
-        text-color        = mkLiteral "#000000";
+        text-color        = mkLiteral "#0f0f0f";
       };
       "#textbox-prompt-colon" = {
         expand            = mkLiteral "false";
         str               = " =";
       };
       "#entry" = {
-        background-color  = mkLiteral "#000000";
+        background-color  = mkLiteral "#0f0f0f";
         margin            = mkLiteral "20px 0px 0px 10px";
         padding           = mkLiteral "6px";
         placeholder       = "Search";
         placeholder-color = mkLiteral "#45475a";
-        text-color        = mkLiteral "#cdd6f4";
+        text-color        = mkLiteral "#ebdbb2";
       };
       "#listview" = {
-        background-color  = mkLiteral "#000000";
+        background-color  = mkLiteral "#0f0f0f";
         border            = mkLiteral "0px 0px 0px";
-        columns           = mkLiteral "2";
+        columns           = mkLiteral "1";
         lines             = mkLiteral "5";
         margin            = mkLiteral "0px 20px 0px 20px";
         padding           = mkLiteral "6px 0px 0px";
       };
       "#element" = {
-        background-color  = mkLiteral "#000000";
+        background-color  = mkLiteral "#0f0f0f";
         padding           = mkLiteral "5px";
-        text-color        = mkLiteral "#cdd6f4";
+        text-color        = mkLiteral "#ebdbb2";
       };
       "#element-icon" = {
         size              = mkLiteral "20px";
       };
       "#element selected" = {
-        background-color  = mkLiteral "#89b4fa";
-        border-radius     = mkLiteral "4px";
-        text-color        = mkLiteral "#000000";
+        background-color  = mkLiteral "#458588";
+        border-radius     = mkLiteral "0px";
+        text-color        = mkLiteral "#0f0f0f";
       };
       "#mode-switcher" = {
         spacing           = mkLiteral "0";
       };
       "#button" = {
-        background-color  = mkLiteral "#000000";
+        background-color  = mkLiteral "#0f0f0f";
         horizontal-align  = mkLiteral "0.5";
         padding           = mkLiteral "10px";
         text-color        = mkLiteral "#6c7086";
         vertical-align    = mkLiteral "0.5";
       };
       "#button selected" = {
-        background-color  = mkLiteral "#000000";
-        text-color        = mkLiteral "#89b4fa";
+        background-color  = mkLiteral "#0f0f0f";
+        text-color        = mkLiteral "#458588";
       };
       "#message" = {
-        background-color  = mkLiteral "#000000";
-        border-radius     = mkLiteral "4px";
+        background-color  = mkLiteral "#0f0f0f";
+        border-radius     = mkLiteral "0px";
         margin            = mkLiteral "2px";
         padding           = mkLiteral "2px";
       };
       "#textbox" = {
-        background-color  = mkLiteral "#000000";
+        background-color  = mkLiteral "#0f0f0f";
         margin            = mkLiteral "20px 0px 0px 20px";
         padding           = mkLiteral "6px";
-        text-color        = mkLiteral "#89b4fa";
+        text-color        = mkLiteral "#458588";
       };
     };
   };
