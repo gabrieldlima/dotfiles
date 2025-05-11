fb59cf12e33cf1f782f15bffb33230b300a96d1d
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-05-11 18:46:43 -0300

    bat: update theme

--- home/bat/default.nix
+++ home/bat/default.nix
@@ -8,24 +8,23 @@
 # ╚═════════════════════════╝
 
 {
-  pkgs,
   ...
 }: {
   programs.bat = {
     enable = true;
     themes = {
-      CatppuccinMocha = {
-        src = pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "bat";
-          rev = "d714cc1";
-          sha256 = "sha256-Q5B4NDrfCIK3UAMs94vdXnR42k4AXCqZz6sRn8bzmf4=";
-        };
-        file = "themes/Catppuccin Mocha.tmTheme";
-      };
+      # CatppuccinMocha = {
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "catppuccin";
+      #     repo = "bat";
+      #     rev = "d714cc1";
+      #     sha256 = "sha256-Q5B4NDrfCIK3UAMs94vdXnR42k4AXCqZz6sRn8bzmf4=";
+      #   };
+      #   file = "themes/Catppuccin Mocha.tmTheme";
+      # };
     };
     config = {
-      theme = "CatppuccinMocha";
+      theme = "gruvbox-dark";
       style = "header-filename,header-filesize,grid";
       wrap = "never";
     };
