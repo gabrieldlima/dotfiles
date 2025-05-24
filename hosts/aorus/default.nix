07d7edb6879a2f0830cc3842ab28789451a1655c
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-05-24 19:53:22 -0300

    Comment out gestures code for now, until fix erros from new Hyprland version

--- hosts/aorus/default.nix
+++ hosts/aorus/default.nix
@@ -59,6 +59,7 @@
 
 
     udev.extraRules = ''
+      KERNEL=="hidraw*", KERNELS=="*054C:0CE6*", MODE="0660", TAG+="uaccess"
       KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0660", TAG+="uaccess"
     '';
 
