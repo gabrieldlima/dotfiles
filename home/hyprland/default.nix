8e7ef45f4ae0c727dac3f194f305189af6ebf618
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-07-18 19:43:26 -0300

    hyprland: change border size

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -85,7 +85,7 @@ in {
       # GENERAL                                                                #
       # ====================================================================== #
       general = {
-        border_size                 = 2;
+        border_size                 = 3;
         gaps_in                     = 5;
         gaps_out                    = 10;
         "col.active_border"         = "${blue}";
