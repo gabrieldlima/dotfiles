843835a17ae5d05f16d07fe9701af981b2bfee74
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-08-16 16:47:21 -0300

    ghostty: update config

--- home/ghostty/default.nix
+++ home/ghostty/default.nix
@@ -12,7 +12,7 @@
       mouse-hide-while-typing = true;
       clipboard-paste-protection = false;
 
-      window-decoration = false;
+      window-decoration = "none";
       window-theme = "ghostty";
       window-width = 120;
       window-height = 35;
