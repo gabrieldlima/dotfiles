04e5a57feb9bb513440da40523c2f4a634ebfb08
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-08-27 18:46:01 -0300

    Disable zen-browser flake

--- flake.nix
+++ flake.nix
@@ -16,10 +16,10 @@
 
     stylix.url = "github:danth/stylix";
 
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # zen-browser = {
+    #   url = "github:0xc000022070/zen-browser-flake";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs = {
