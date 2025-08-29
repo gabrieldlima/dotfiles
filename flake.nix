2dadddd706846f3ecff2edef37c1756ceb5e6c49
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-08-29 19:42:39 -0300

    flake.nix: update comments

--- flake.nix
+++ flake.nix
@@ -9,12 +9,12 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    stylix.url = "github:danth/stylix";
+
     # quickshell = {
     #   url = "github:quickshell-mirror/quickshell";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
-
-    stylix.url = "github:danth/stylix";
 
     # zen-browser = {
     #   url = "github:0xc000022070/zen-browser-flake";
