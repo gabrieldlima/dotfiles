fdc2c17a9ad3638b94bece8523cedf504ea53ace
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-08-29 19:44:41 -0300

    alacritty: change theme and fonts

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -11,10 +11,10 @@
   ...
 }: {
   programs.alacritty = {
-    enable = false;
+    enable = true;
     settings = {
       # GENERAL
-      shell = "fish";
+      terminal.shell = "fish";
 
       # ENV
       env = {
@@ -33,98 +33,50 @@
       # FONT
       font = {
         normal = {
-          family = "FiraCode Nerd Font";
+          family = "Iosevka";
           style = "Regular";
         };
         bold = {
-          family = "FiraCode Nerd Font";
+          family = "Iosevka";
           style = "Bold";
         };
         italic = {
-          family = "FiraCode Nerd Font";
+          family = "Iosevka";
           style = "Italic";
         };
         bold_italic = {
-          family = "FiraCode Nerd Font";
+          family = "Iosevka";
           style = "Bold Italic";
         };
-        size = 11;
+        size = 12;
       };
 
       # COLORS
       colors = {
         primary = {
           background = "#000000";
-          foreground = "#cdd6f4";
-          dim_foreground = "#7f849c";
-          bright_foreground = "#cdd6f4";
-        };
-        cursor = {
-          text = "#1e1e2e";
-          cursor = "#f5e0dc";
-        };
-        vi_mode_cursor = {
-          text = "#1e1e2e";
-          cursor = "#b4befe";
-        };
-        search.matches = {
-          foreground = "#1e1e2e";
-          background = "#a6adc8";
-        };
-        search.focused_match = {
-          foreground = "#1e1e2e";
-          background = "#a6e3a1";
-        };
-        footer_bar = {
-          foreground = "#1e1e2e";
-          background = "#a6adc8";
-        };
-        hints.start = {
-          foreground = "#1e1e2e";
-          background = "#f9e2af";
-        };
-        hints.end = {
-          foreground = "#1e1e2e";
-          background = "#a6adc8";
-        };
-        selection = {
-          text = "#1e1e2e";
-          background = "#f5e0dc";
+          foreground = "#ebdbb2";
         };
         normal = {
-          black = "#45475a";
-          red = "#f38ba8";
-          green = "#a6e3a1";
-          yellow = "#f9e2af";
-          blue = "#89b4fa";
-          magenta = "#f5c2e7";
-          cyan = "#94e2d5";
-          white = "#bac2de";
+          black   = "#282828";
+          red     = "#cc241d";
+          green   = "#98971a";
+          yellow  = "#d79921";
+          blue    = "#458588";
+          magenta = "#b16286";
+          cyan    = "#689d6a";
+          white   = "#a89984";
         };
         bright = {
-          black = "#585b70";
-          red = "#f38ba8";
-          green = "#a6e3a1";
-          yellow = "#f9e2af";
-          blue = "#89b4fa";
-          magenta = "#f5c2e7";
-          cyan = "#94e2d5";
-          white = "#a6adc8";
+          black   = "#928374";
+          red     = "#fb4934";
+          green   = "#b8bb26";
+          yellow  = "#fabd2f";
+          blue    = "#83a598";
+          magenta = "#d3869b";
+          cyan    = "#8ec07c";
+          white   = "#ebdbb2";
         };
-        dim = {
-          black = "#45475a";
-          red = "#f38ba8";
-          green = "#a6e3a1";
-          yellow = "#f9e2af";
-          blue = "#89b4fa";
-          magenta = "#f5c2e7";
-          cyan = "#94e2d5";
-          white = "#bac2de";
-        };
-        # indexed_colors = [
-        # { index = 16; color = "#fab387"; }
-        # { index = 17; color = "#f5e0dc"; }
-        # ];
       };
 
       # MOUSE
