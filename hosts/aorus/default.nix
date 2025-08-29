3f92060c2eb40c0837f0161e8e034452c356c5d1
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-08-29 19:43:01 -0300

    Fix blank lines

--- hosts/aorus/default.nix
+++ hosts/aorus/default.nix
@@ -57,7 +57,6 @@
     #   # };
     # };
 
-
     udev.extraRules = ''
       KERNEL=="hidraw*", KERNELS=="*054C:0CE6*", MODE="0660", TAG+="uaccess"
       KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0660", TAG+="uaccess"
