1161bbd959857ac833d89366f158ade2949e8aec
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-11-21 20:51:46 -0300

    i3wm: set caps to ctrl

--- home/i3wm/default.nix
+++ home/i3wm/default.nix
@@ -52,8 +52,8 @@
         { command = "picom"; notification = false; }
         { command = "xwallpaper --stretch ${config.home.homeDirectory}/pictures/wallpapers/wallpaper.jpg"; notification = false; }
         { command = "xrdb -load ${config.home.homeDirectory}/.Xresources"; notification = false; }
-        # { command = "setxkbmap -option ctrl:nocaps"; notification = false; }
-        { command = "setxkbmap -option caps:escape"; notification = false; }
+        { command = "setxkbmap -option ctrl:nocaps"; notification = false; }
+        # { command = "setxkbmap -option caps:escape"; notification = false; }
         { command = "dunst"; notification = false; }
       ];
 
