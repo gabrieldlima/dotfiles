cce17c27a8a2584399406ae24273403e5611040a
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-11-21 20:51:32 -0300

    kitty: update config

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -13,19 +13,23 @@
   programs.kitty = {
     enable = true;
 
-    font = {
-      name = "Iosevka Term Slab Medium";
-      size = 11;
-    };
+    extraConfig = ''
+      font_family family="Iosevka"
+      bold_font        auto
+      italic_font      auto
+      bold_italic_font auto
+      font_size 11.0
+    '';
 
     settings = {
       # Cursor customization
-      cursor            = "#F5E0DC";
-      cursor_text_color = "#1E1E2E";
-      cursor_shape      = "beam";
+      cursor            = "#bdae93";
+      cursor_text_color = "#665c54";
+      cursor_shape      = "block";
+      cursor_trail      = 1;
 
       # Mouse
-      url_color      = "#F5E0DC";
+      url_color      = "#458588";
       copy_on_select = true;
       open_url_with  = "qutebrowser";
 
@@ -34,76 +38,63 @@
 
       # Window layout
       remember_window_size = false;
-      initial_window_height = 500;
       initial_window_width = 1000;
+      initial_window_height = 700;
       window_margin_width = 5;
       confirm_os_window_close = 0;
 
       # Tab bar
-      active_tab_foreground   = "#11111B";
-      active_tab_background   = "#050508";
-      inactive_tab_foreground = "#CDD6F4";
-      inactive_tab_background = "#181825";
-      tab_bar_background      = "#11111B";
+      active_tab_foreground   = "#eeeeee";
+      active_tab_background   = "#d65d0e";
+      inactive_tab_foreground = "#ebdbb2";
+      inactive_tab_background = "#171a1a";
+      tab_bar_background      = "#000000";
 
       # The shell program to execute
       shell = "fish";
 
       # Color scheme
-      background_opacity   = "0.95";
-      foreground           = "#CDD6F4";
+      background_opacity   = "1";
+      foreground           = "#ebdbb2";
       background           = "#000000";
-      selection_foreground = "#1E1E2E";
-      selection_background = "#F5E0DC";
-
-      # Kitty window border colors
-      active_border_color   = "#B4BEFE";
-      inactive_border_color = "#6C7086";
-      bell_border_color     = "#F9E2AF";
+      selection_foreground = "#ebdbb2";
+      selection_background = "#d65d0e";
 
       # OS Window titlebar colors
       wayland_titlebar_color = "system";
 
-      # Colors for marks (marked text in the terminal)
-      mark1_foreground = "#1E1E2E";
-      mark1_background = "#B4BEFE";
-      mark2_foreground = "#1E1E2E";
-      mark2_background = "#CBA6F7";
-      mark3_foreground = "#1E1E2E";
-      mark3_background = "#74C7EC";
-
-      # The 16 terminal colors";
+      # Gruvbox dark hard
       # black
-      color0 = "#45475A";
-      color8 = "#585B70";
+      color0 = "#3c3836";
+      color8 = "#928374";
 
       # red
-      color1 = "#F38BA8";
-      color9 = "#F38BA8";
+      color1 = "#cc241d";
+      color9 = "#fb4934";
 
       # green
-      color2  = "#A6E3A1";
-      color10 = "#A6E3A1";
+      color2  = "#98971a";
+      color10 = "#b8bb26";
 
       # yellow
-      color3  = "#F9E2AF";
-      color11 = "#F9E2AF";
+      color3  = "#d79921";
+      color11 = "#fabd2f";
 
       # blue
-      color4  = "#89B4FA";
-      color12 = "#89B4FA";
+      color4  = "#458588";
+      color12 = "#83a598";
 
       # magenta
-      color5  = "#F5C2E7";
-      color13 = "#F5C2E7";
+      color5  = "#b16286";
+      color13 = "#d3869b";
 
       # cyan
-      color6  = "#94E2D5";
-      color14 = "#94E2D5";
+      color6  = "#689d6a";
+      color14 = "#8ec07c";
 
       # white
-      color7  = "#BAC2DE";
-      color15 = "#A6ADC8";
+      color7  = "#a89984";
+      color15 = "#fbf1c7";
     };
   };
 }
