c0ae677ca806076239bec94e25a8f524cffac00c
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-12-26 21:01:39 -0300

    wezterm: set command palette colors/font

--- config/wezterm/config.lua
+++ config/wezterm/config.lua
@@ -68,6 +68,12 @@ M.colors = {
     },
 }
 
+M.command_palette_bg_color = "#000000"
+M.command_palette_fg_color = "#ebdbb2"
+M.command_palette_font = wezterm.font "JetBrains Mono"
+M.command_palette_font_size = 12
+M.command_palette_rows = 14
+
 -- Launcher
 -- =============================================================================
 M.launch_menu = {
