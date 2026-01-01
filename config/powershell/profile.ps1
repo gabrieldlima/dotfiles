5c8fe3b4aa84207326e9c952e20386368d9ecf7e
Gabriel Lima <gabrielgsdlima@gmail.com>
2026-01-01 13:49:46 -0300

    powershell: disable starship because slow startup

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -1,13 +1,23 @@
 Set-PSReadLineOption -EditMode Emacs
 
-function Invoke-Starship-PreCommand {
+function prompt {
   $loc = $executionContext.SessionState.Path.CurrentLocation;
-  $prompt = "$([char]27)]9;12$([char]7)"
-  if ($loc.Provider.Name -eq "FileSystem")
-  {
-    $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+
+  $out = ""
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
   }
-  $host.ui.Write($prompt)
+  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  return $out
 }
 
-Invoke-Expression (&starship init powershell)
+# function Invoke-Starship-PreCommand {
+#   $loc = $executionContext.SessionState.Path.CurrentLocation;
+#   $prompt = "$([char]27)]9;12$([char]7)"
+#   if ($loc.Provider.Name -eq "FileSystem")
+#   {
+#     $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+#   }
+#   $host.ui.Write($prompt)
+# }
+# Invoke-Expression (&starship init powershell)
