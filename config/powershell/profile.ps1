187e24130870be98b77330ff48b9e6d0774f6623
Gabriel Lima <gabrielgsdlima@gmail.com>
2026-01-01 12:57:55 -0300

    powershell: workaround to open tabs/panes in the same directory

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -1,1 +1,12 @@
 Set-PSReadLineOption -EditMode Emacs
+
+function prompt {
+    $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+    $out = ""
+        if ($loc.Provider.Name -eq "FileSystem") {
+            $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+        }
+    $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+    return $out
+}
