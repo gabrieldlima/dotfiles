a264179241573481fad3d5ad1cf82adb9c3f1f1a
Gabriel Lima <gabrielgsdlima@gmail.com>
2026-01-01 13:29:31 -0300

    powershell: upate config

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -1,12 +1,13 @@
 Set-PSReadLineOption -EditMode Emacs
 
-function prompt {
-    $loc = $executionContext.SessionState.Path.CurrentLocation;
-
-    $out = ""
-        if ($loc.Provider.Name -eq "FileSystem") {
-            $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-        }
-    $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
-    return $out
+function Invoke-Starship-PreCommand {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+  $prompt = "$([char]27)]9;12$([char]7)"
+  if ($loc.Provider.Name -eq "FileSystem")
+  {
+    $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $host.ui.Write($prompt)
 }
+
+Invoke-Expression (&starship init powershell)
