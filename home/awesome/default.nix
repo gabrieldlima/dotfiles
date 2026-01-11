956df704cba869f695762d76d2d5c0f7b322d739
Gabriel Lima <gabrielgsdlima@gmail.com>
2026-01-11 10:48:42 -0300

    awesome: update rev

--- home/awesome/default.nix
+++ home/awesome/default.nix
@@ -13,23 +13,38 @@
 }:
 let
   awesome-git = pkgs.awesome.overrideAttrs (oa: {
-    version = "git-691e36425a645e54402cb04efc4c2b00d73051bd";
+    version = "git";
     src = pkgs.fetchFromGitHub {
       owner = "awesomeWM";
       repo = "awesome";
-      rev = "691e36425a645e54402cb04efc4c2b00d73051bd";
-      hash = "sha256-IN5sNBDoC6CtBzr3Qp8S9r0rfqR2CD/maGB1aiZdRE4=";
+      rev = "cab3e81dc6071e3c1c4bd15cf8fab91236c7f2bd";
+      hash = "sha256-g4cSjASTulAIXM1nSRGqdBRNOyUDl8A+WlJhF3duq+E=";
     };
 
-    patches = [
-      ./patches/tasklist.patch
-    ];
+    patches = [];
 
     postPatch = ''
       patchShebangs tests/examples/_postprocess.lua
     '';
   });
 in {
+  imports = [
+    ../picom
+    ../xresources
+  ];
+
+  home = {
+    file.".config/x11/xinitrc".text = ''
+      exec awesome
+    '';
+
+    packages = [
+      pkgs.sxiv
+      pkgs.xsel
+      pkgs.xwallpaper
+    ];
+  };
+
   xsession.windowManager.awesome = {
     enable = true;
     package = awesome-git;
