1608d75438699c9f48e683076fcdb4cb8584fc75
Gabriel Lima <gabrielgsdlima@gmail.com>
2026-02-23 20:23:52 -0300

    change mouse cursor

--- home/stylix/default.nix
+++ home/stylix/default.nix
@@ -43,11 +43,11 @@
     };
 
     cursor = {
-      # name = "GoogleDot-White";
-      # package = pkgs.google-cursor;
-      name = "Bibata-Modern-Ice-Right";
-      package = pkgs.bibata-cursors;
-      size = 42;
+      name = "GoogleDot-White";
+      package = pkgs.google-cursor;
+      # name = "Bibata-Modern-Ice-Right";
+      # package = pkgs.bibata-cursors;
+      size = 62;
     };
 
     icons = {
