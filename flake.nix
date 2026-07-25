72b313e739173c0e6f7b316242659cfdd9c2be4c
Gabriel Lima <gabrielgsdlima@gmail.com>
2026-07-25 12:13:07 -0300

    update flake

--- flake.nix
+++ flake.nix
@@ -23,10 +23,10 @@
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
 
-    # zen-browser = {
-    #   url = "github:0xc000022070/zen-browser-flake";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
